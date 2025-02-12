2815c13c50b5396d80f6cc915cc20a6d221b3e28
dolen3 <danolen89@gmail.com>
2025-02-12 15:39:16 -0500

    Created DAO and User entity

--- src/main/java/com/revature/P1Backend/P1BackendApplication.java
+++ src/main/java/com/revature/P1Backend/P1BackendApplication.java
@@ -2,13 +2,23 @@ package com.revature.P1Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EntityScan("com.revature.P1Backend.models")
+@ComponentScan("com.revature.P1Backend")
 public class P1BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(P1BackendApplication.class, args);
+
+		System.out.println("Employee Reimbursement System");
 	}
+
+
+
 
 }
 
---
+++ src/main/java/com/revature/P1Backend/models/User.java
@@ -0,0 +1,72 @@
+package com.revature.P1Backend.models;
+import org.springframework.stereotype.Component;
+import jakarta.persistence.*;
+
+@Component
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    private String password;
+    private String role;
+
+    //boilerplate code
+    public User() {
+    }
+
+    public User(int userId, String username, String password, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
