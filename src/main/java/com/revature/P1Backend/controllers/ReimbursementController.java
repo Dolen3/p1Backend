6119770f2b7bc5629f933e0f1aeccdb8ff366bad
Benjamin Huh <bjhuh@wm.edu>
2025-02-15 15:42:33 -0500

    New Branch Initial Commit

--- src/main/java/com/revature/P1Backend/controllers/ReimbursementController.java
+++ src/main/java/com/revature/P1Backend/controllers/ReimbursementController.java
@@ -1,4 +1,9 @@
 package com.revature.P1Backend.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class ReimbursementController {
 }
